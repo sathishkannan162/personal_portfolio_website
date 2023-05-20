7c296feeba89f6286e6e9dc6ba30ba55207d41c0
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-21 02:04:03 +0530

    feat: add info in about page

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -1,16 +1,39 @@
-import { Heading, Text } from '@chakra-ui/react';
+import { Heading, Text, Image, Box, Link } from '@chakra-ui/react';
 import PageContainer from './PageContainer';
+import myPhoto from './assets/my_photo.jpg';
 
 export default function AboutPage() {
   return (
     <PageContainer>
       <Heading>About Me</Heading>
       <br />
-      <Text>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum soluta
-        obcaecati quia asperiores fugiat eveniet. Neque dicta similique,
-        pariatur fugit placeat voluptate quod minima. Incidunt quibusdam atque
-        minus neque reprehenderit.
+      <Box maxW="400px" maxH="400px" float="left" pr={50}>
+        <Image src={myPhoto} borderRadius={9} />
+      </Box>
+      <Text fontSize={'lg'}>
+        <Text>
+          I am Sathish Kannan. I recently graduated from 🎓IIT Bombay. I am a
+          self taught🤸 full stack developer.
+          <br />
+          <br />
+          Currently, I am building products that integrates with 🤖AI.
+          <br />
+          <br />
+          Checkout my personal blog:{' '}
+          <Link href="https://sathishkannan162.github.io" color={'teal'}>
+            https://sathishkannan162.github.io
+          </Link>
+        </Text>
+        <br />
+        <Text>
+          Checkout my
+          <ul>
+            <li>Twitter</li>
+            <li>LinkedIn</li>
+            <li>Instagram</li>
+            <li>Facebook</li>
+          </ul>
+        </Text>
       </Text>
     </PageContainer>
   );
--- src/PageContainer.jsx
+++ src/PageContainer.jsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 
 export default function PageContainer({ children }) {
   return (
-    <Box maxW="1000px" mx="auto" my="30px">
+    <Box maxW="1000px" height={'70vh'} mx="auto" my="30px">
       {/* my default maxW was 1200px but reduced to 1000px here */}
       {children}
     </Box>
