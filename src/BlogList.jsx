7b27c0244afdbb1d58b43bcbee424226d7a57319
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-21 01:21:04 +0530

    fix: latest projects appear first

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -8,45 +8,7 @@ import {
   Heading,
 } from '@chakra-ui/react';
 import projects from './projectInfoArray.js';
-
-const dataList = [
-  {
-    id: 1,
-    title: 'Chlropeth Maps with D3.js',
-    authorName: 'Ali',
-    content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-  },
-  {
-    id: 2,
-    title: 'Rails ActiveAdmin',
-    authorName: 'Ali',
-    content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-  },
-  {
-    id: 3,
-    title: 'Rails ActiveAdmin',
-    authorName: 'Ali',
-    content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-  },
-  {
-    id: 4,
-    title: 'Rails ActiveAdmin',
-    authorName: 'Ali',
-    content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-  },
-  {
-    id: 5,
-    title: 'Rails ActiveAdmin',
-    authorName: 'Ali',
-    content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-  },
-  {
-    id: 6,
-    title: 'Rails ActiveAdmin',
-    authorName: 'Ali',
-    content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-  },
-];
+let reverseProjects = projects.reverse();
 
 const BlogCards = (props) => {
   const { title, count, skip } = props;
@@ -54,7 +16,7 @@ const BlogCards = (props) => {
     <Container maxWidth="1200px" mx="auto" my="auto" p={{ base: 5, md: 10 }}>
       <Heading py={'3'}>{title}</Heading>
       <SimpleGrid columns={[1, 2, 3]} spacing="15px">
-        {projects.slice(skip, count + skip).map((blog) => {
+        {reverseProjects.slice(skip, count + skip).map((blog) => {
           return (
             <Box position="relative" key={blog.projectID}>
               <Box
