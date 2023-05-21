5f0d634d1c045684091b2c7348a52f701b9142a6
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-22 00:45:28 +0530

    feat: add meta data for link sharing

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,24 +2,40 @@ import { Outlet } from 'react-router-dom';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
+import { Helmet } from 'react-helmet';
 
 // const customTheme = extendTheme({
-  // fonts: {
-  //   body: 'Arial, sans-serif',
-  //   heading: 'Georgia, serif',
-  // },
-  // styles: {
-  //   global: {
-  //     body: {
-  //       backgroundColor: "#f8fafc"
-  //     },
-  //   },
-  // },
+// fonts: {
+//   body: 'Arial, sans-serif',
+//   heading: 'Georgia, serif',
+// },
+// styles: {
+//   global: {
+//     body: {
+//       backgroundColor: "#f8fafc"
+//     },
+//   },
+// },
 // });
 
 export default function Root() {
   return (
     <ChakraProvider>
+      <Helmet>
+        <meta property="og:title" content="Sathish Kannan Portfolio" />
+        <meta
+          property="og:description"
+          content="My portfolio website showing my projects and my contact informatio"
+        />
+        <meta
+          property="og:image"
+          content="https://projectimages.netlify.app/programmer_under_tree.jpeg"
+        />
+        <meta
+          property="og:url"
+          content="https://sathishkannanportfolio.netlify.app"
+        />
+      </Helmet>
       {/* <ChakraProvider theme={customTheme}> */}
       <Navbar />
       <Outlet />
