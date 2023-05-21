92a0fc04a52d2c38d1073fc37635d31c417e28ea
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-21 22:36:52 +0530

    fix: remove unwanted code

--- src/ProjectHeroCard.jsx
+++ src/ProjectHeroCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, chakra, Button, Image } from '@chakra-ui/react';
+import { Box, Flex, chakra, Button } from '@chakra-ui/react';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 
 const ProjectHeroCard = (props) => {
