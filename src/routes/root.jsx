17cb495528efeb074d17b3b20f120823717ab505
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-22 01:03:44 +0530

    docs: remove react-helment as it not needed
    docs: remove react-helment as it not needed

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
-import { Helmet } from 'react-helmet';
 
 // const customTheme = extendTheme({
 // fonts: {
@@ -21,21 +20,6 @@ import { Helmet } from 'react-helmet';
 export default function Root() {
   return (
     <ChakraProvider>
-      <Helmet>
-        <meta property="og:title" content="Sathish Kannan Portfolio" />
-        <meta
-          property="og:description"
-          content="My portfolio website showing my projects and my contact informatio"
-        />
-        <meta
-          property="og:image"
-          content="https://projectimages.netlify.app/programmer_under_tree.jpeg"
-        />
-        <meta
-          property="og:url"
-          content="https://sathishkannanportfolio.netlify.app"
-        />
-      </Helmet>
       {/* <ChakraProvider theme={customTheme}> */}
       <Navbar />
       <Outlet />
