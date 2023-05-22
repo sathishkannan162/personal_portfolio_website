2567040fc98a7406933d4408c42b56027d6eefc3
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-22 13:03:11 +0530

    feat: filter projects by tag on projects page

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -34,12 +34,18 @@ function getUniqueTags(projects) {
 
   return Array.from(tagsSet);
 }
+
+function filterProjectsByTag(projects, tag) {
+  if (tag === 'All') {
+    return projects;
+  }
+  return projects.filter((project) => project.tags.includes(tag));
+}
 const BlogCards = (props) => {
   const { title, count, skip, interactive } = props;
   const [tagSelect, setTagSelect] = useState('All');
   const handleSelectTag = (e) => {
     setTagSelect(e.target.value);
-    console.log(e.target.value);
   };
   const tags = getUniqueTags(projects);
   return (
@@ -61,74 +67,76 @@ const BlogCards = (props) => {
         </Box>
       )}
       <SimpleGrid columns={[1, 2, 3]} spacing="15px">
-        {projects.slice(skip, count + skip).map((blog) => {
-          return (
-            <Box position="relative" key={blog.projectID}>
-              <RiseAnimation>
-                <Box
-                  fontSize="sm"
-                  position="absolute"
-                  right="5px"
-                  margin="5px"
-                  zIndex="1"
-                ></Box>
-                <a href={blog.link} target="_blank">
+        {filterProjectsByTag(projects, tagSelect)
+          .slice(skip, count + skip)
+          .map((blog) => {
+            return (
+              <Box position="relative" key={blog.projectID}>
+                <RiseAnimation>
                   <Box
-                    borderWidth="1px"
-                    rounded="lg"
-                    overflow="hidden"
-                    position="relative"
-                    _hover={{ shadow: 'md' }}
-                  >
-                    <Box maxH="160px" p={4} overflow="hidden">
-                      <Image
-                        src={blog.thumbnail}
-                        overflow="clip"
-                        mx="auto"
-                        alt="Blog image"
-                        loading="lazy"
-                      />
-                    </Box>
-                    <Box px={{ base: 4, lg: 6 }}>
-                      <Box d="flex" alignItems="baseline">
-                        <Box
-                          fontWeight="semibold"
-                          as="h2"
-                          letterSpacing="wide"
-                          textTransform="uppercase"
-                        >
-                          {truncateBlogTitle(blog.title, 30)}
+                    fontSize="sm"
+                    position="absolute"
+                    right="5px"
+                    margin="5px"
+                    zIndex="1"
+                  ></Box>
+                  <a href={blog.link} target="_blank">
+                    <Box
+                      borderWidth="1px"
+                      rounded="lg"
+                      overflow="hidden"
+                      position="relative"
+                      _hover={{ shadow: 'md' }}
+                    >
+                      <Box maxH="160px" p={4} overflow="hidden">
+                        <Image
+                          src={blog.thumbnail}
+                          overflow="clip"
+                          mx="auto"
+                          alt="Blog image"
+                          loading="lazy"
+                        />
+                      </Box>
+                      <Box px={{ base: 4, lg: 6 }}>
+                        <Box d="flex" alignItems="baseline">
+                          <Box
+                            fontWeight="semibold"
+                            as="h2"
+                            letterSpacing="wide"
+                            textTransform="uppercase"
+                          >
+                            {truncateBlogTitle(blog.title, 30)}
+                          </Box>
                         </Box>
+                        <Box>
+                          <Box color="gray.600" fontSize="sm"></Box>
+                        </Box>
+                        <Text
+                          mt="1"
+                          fontWeight="semibold"
+                          noOfLines={3}
+                          lineHeight="tight"
+                          color="gray.600"
+                          fontSize="sm"
+                        >
+                          {blog.content}
+                        </Text>
+                        <HStack py={2}>
+                          {blog.tags.slice(0, 3).map((tag, index) => {
+                            return (
+                              <Tag key={index} colorScheme={'teal'}>
+                                <Text fontSize={12}>{tag}</Text>
+                              </Tag>
+                            );
+                          })}
+                        </HStack>
                       </Box>
-                      <Box>
-                        <Box color="gray.600" fontSize="sm"></Box>
-                      </Box>
-                      <Text
-                        mt="1"
-                        fontWeight="semibold"
-                        noOfLines={3}
-                        lineHeight="tight"
-                        color="gray.600"
-                        fontSize="sm"
-                      >
-                        {blog.content}
-                      </Text>
-                      <HStack py={2}>
-                        {blog.tags.slice(0, 3).map((tag, index) => {
-                          return (
-                            <Tag key={index} colorScheme={'teal'}>
-                              <Text fontSize={12}>{tag}</Text>
-                            </Tag>
-                          );
-                        })}
-                      </HStack>
                     </Box>
-                  </Box>
-                </a>
-              </RiseAnimation>
-            </Box>
-          );
-        })}
+                  </a>
+                </RiseAnimation>
+              </Box>
+            );
+          })}
       </SimpleGrid>
     </Container>
   );
