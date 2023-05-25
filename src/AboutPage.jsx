f956aa618695cab8dc4f7e6af7ed1e7304b54e6f
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-25 11:45:21 +0530

    style: change padding for about and contact page, about page image alignment change

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -12,49 +12,58 @@ export default function AboutPage() {
       <Box
         maxW="400px"
         maxH="400px"
+        mx="auto"
         float={{ base: 'none', md: 'left' }}
-        pr={50}
+        pr={{ base: 0, md: 50 }}
+        pl={{ base: 0, md: 0 }}
       >
-        <ImageWithPlaceholder src={myPhoto} borderRadius={9} loading="lazy" />
+        <ImageWithPlaceholder
+          mx="auto"
+          src={myPhoto}
+          borderRadius={9}
+          loading="lazy"
+        />
       </Box>
-      <Text fontSize={'lg'}>
-        I am Sathish Kannan. I recently graduated from 🎓IIT Bombay. I am a self
-        taught🤸 full stack developer.
-        <br />
-        <br />I have good technical proficienty in{' '}
-        <strong>
-          NextJs, React, Express, Nodejs and Data visualisation with D3.js{' '}
-        </strong>
-        <br />
-        <br />
-        Currently, I am building products that integrates with 🤖AI.
-        <br />
-        <br />
-        Checkout my personal blog:{' '}
-        <StandardLink href="https://sathishkannan162.github.io">
-          {' '}
-          https://sathishkannan162.github.io
-        </StandardLink>
-        <br />
-        <br />
-        Checkout my{' '}
-        <StandardLink href="https://www.linkedin.com/in/sathish-kannan-a48a39166/">
-          LinkedIn
-        </StandardLink>
-        ,{' '}
-        <StandardLink href="https://twitter.com/Sathish06030039">
-          Twitter
-        </StandardLink>
-        ,{' '}
-        <StandardLink href="https://www.instagram.com/satishkannan162/">
-          Instagram
-        </StandardLink>{' '}
-        and{' '}
-        <StandardLink href="https://www.facebook.com/sathishkannan.m.3">
-          Facebook
-        </StandardLink>
-        .
-      </Text>
+      <Box pt={{ base: 5, md: 0 }}>
+        <Text fontSize={'lg'}>
+          I am Sathish Kannan. I recently graduated from 🎓IIT Bombay. I am a
+          self taught🤸 full stack developer.
+          <br />
+          <br />I have good technical proficienty in{' '}
+          <strong>
+            NextJs, React, Express, Nodejs and Data visualisation with D3.js{' '}
+          </strong>
+          <br />
+          <br />
+          Currently, I am building products that integrates with 🤖AI.
+          <br />
+          <br />
+          Checkout my personal blog:{' '}
+          <StandardLink href="https://sathishkannan162.github.io">
+            {' '}
+            https://sathishkannan162.github.io
+          </StandardLink>
+          <br />
+          <br />
+          Checkout my{' '}
+          <StandardLink href="https://www.linkedin.com/in/sathish-kannan-a48a39166/">
+            LinkedIn
+          </StandardLink>
+          ,{' '}
+          <StandardLink href="https://twitter.com/Sathish06030039">
+            Twitter
+          </StandardLink>
+          ,{' '}
+          <StandardLink href="https://www.instagram.com/satishkannan162/">
+            Instagram
+          </StandardLink>{' '}
+          and{' '}
+          <StandardLink href="https://www.facebook.com/sathishkannan.m.3">
+            Facebook
+          </StandardLink>
+          .
+        </Text>
+      </Box>
     </PageContainer>
   );
 }
--- src/PageContainer.jsx
+++ src/PageContainer.jsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 
 export default function PageContainer({ children }) {
   return (
-    <Box maxW="1000px" height={{base: 'auto', md: '70vh'}} mx="auto" my="30px" px="10px">
+    <Box maxW="1000px" height={{base: 'auto', md: '70vh'}} mx="auto" my="30px" px="20px">
       {/* my default maxW was 1200px but reduced to 1000px here */}
       {children}
     </Box>
