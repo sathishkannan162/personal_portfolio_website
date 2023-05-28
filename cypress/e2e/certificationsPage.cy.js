c04ede5a7c80c5cf71c0e177c4d1a1adb759124c
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-29 02:46:12 +0530

    test: certifications page e2e

---
+++ cypress/e2e/certificationsPage.cy.js
@@ -0,0 +1,62 @@
+// describe('Certifications page', () => {
+//   it('passes', () => {
+//   })
+// })
+import certificationsInfoArray from '../../src/data/certificationsInfoArray';
+
+describe('Certifications Page', () => {
+  beforeEach(() => {
+    cy.visit('/certifications'); // Assuming the certifications page URL is '/certifications'
+    // cy.wait(500);
+  });
+
+  it('displays the heading', () => {
+    cy.contains('Certifications').should('be.visible');
+  });
+
+  it('displays the certificate cards', () => {
+    cy.get('[data-test="certificate-card"]').should(
+      'have.length',
+      certificationsInfoArray.length
+    ); // Assuming there are 3 certificates in the certificationsInfoArray
+
+    cy.get('[data-test="certificate-card"]').each(($card, index) => {
+      cy.wrap($card).within(() => {
+        cy.scrollTo(0, 200 * index);
+        cy.get('h2').should('be.visible');
+        cy.get('h2')
+          .first()
+          .should('have.text', certificationsInfoArray[index].name);
+        cy.get('h2')
+          .last()
+          .within(() => {
+            cy.get('p')
+              .first()
+              .should('have.text', certificationsInfoArray[index].name);
+            cy.get('p')
+              .eq(1)
+              .should('have.text', certificationsInfoArray[index].provider);
+            cy.get('p').last().should('have.text', 'Click Card to View');
+          });
+        cy.get('a').should(
+          'have.attr',
+          'href',
+          certificationsInfoArray[index].certificateUrl
+        );
+        // cy.get('img').should('be.visible');
+      });
+    });
+  });
+
+  it('opens the certificate URL in a new tab', () => {
+    cy.get('[data-test="certificate-card"]')
+      .first()
+      .within(() => {
+        cy.get('a').should('have.attr', 'target', '_blank');
+      });
+  });
+  it('scroll smoothly to the bottom', () => {
+    cy.scrollTo('bottom', { duration: 2000 });
+    cy.scrollTo('top', { duration: 2000 });
+  });
+});
