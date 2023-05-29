c660c218d2ec4d98539a2c65bb268db131fdfe5f
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-29 14:54:44 +0530

    fix: baseUrl fixed error in cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:4173',
     experimentalStudio: true,
     // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
